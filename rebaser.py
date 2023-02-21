967c67c9505144dc05ccb2f920aebf0b10de9e46
Christian Baars <Baars@gmx.de>
2023-02-21 21:18:23 +0100

    Update rebaser.py

--- rebaser.py
+++ rebaser.py
@@ -1,4 +1,10 @@
 s = "12345678"
+with open('commit_history.txt') as f:
+    text = f.read()
+    print(text)
 with open('commit_history.txt', 'a') as fd:
     fd.write(f'\n{s}')
 print("Did it",s)
+with open('commit_history.txt') as f:
+    text = f.read()
+    print(text)
