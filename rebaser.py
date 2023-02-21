3049457e2a9709c6f387f34399ba3b7c7e8aab9d
Christian Baars <Baars@gmx.de>
2023-02-21 21:50:06 +0100

    Update rebaser.py

--- rebaser.py
+++ rebaser.py
@@ -3,16 +3,21 @@ import sys
 import json
 
 hash = ""
+oldest_hash = ""
+hash_list = []
 
 with open('env.txt') as f:
     data = json.load(f)
     hash = data["after"]
+    
+with open('commit_history.txt') as f:
+    hash_list = f.readlines()
+    oldest_hash = hash_list[0]
+    print(hash_list,oldest_hash)
 
 with open('commit_history.txt', 'a') as fd:
     fd.write(f'\n{hash}')
     
 print("Did it:",hash)
 
-with open('commit_history.txt') as f:
-    text = f.read()
-    print(text)
+
